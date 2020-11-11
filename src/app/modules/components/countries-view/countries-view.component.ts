e8f6cdb1ce3cccaaedc5509e23da597055749207
pramodk-12 <pramodmanepalli32@gmail.com>
2020-11-11 22:49:14 +0530

    animations

--- src/app/modules/components/countries-view/countries-view.component.ts
+++ src/app/modules/components/countries-view/countries-view.component.ts
@@ -1,10 +1,21 @@
+import { trigger, transition, query, style, stagger, animate } from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
 import { ICountries } from '../../shared/countries';
 
 @Component({
   selector: 'app-countries-view',
   templateUrl: './countries-view.component.html',
-  styleUrls: ['./countries-view.component.css']
+  styleUrls: ['./countries-view.component.css'],
+  animations: [
+    trigger('header', [
+      transition(':enter', [
+        query('*', style({ opacity: 0, transform: 'translateY(-15%)' })),
+        query('*', stagger('50ms', [
+          animate('.3s ease-in', style({ opacity: 1, transform: 'translateY(0)'}))
+          ]))
+        ])
+      ])
+  ]
 })
 export class CountriesViewComponent implements OnInit {
 
--- src/app/modules/components/detail-view/detail-view.component.ts
+++ src/app/modules/components/detail-view/detail-view.component.ts
@@ -1,15 +1,26 @@
+import { trigger, transition, query, style, stagger, animate } from '@angular/animations';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ICountries } from '../../shared/countries';
 
 @Component({
   selector: 'app-detail-view',
   templateUrl: './detail-view.component.html',
-  styleUrls: ['./detail-view.component.css']
+  styleUrls: ['./detail-view.component.css'],
+  animations: [
+    trigger('header', [
+      transition(':enter', [
+        query('*', style({ opacity: 0, transform: 'translateY(-15%)' })),
+        query('*', stagger('50ms', [
+          animate('.3s ease-in', style({ opacity: 1, transform: 'translateY(0)'}))
+          ]))
+        ])
+      ])
+  ]
 })
 export class DetailViewComponent implements OnInit {
 
   @Input() country$: ICountries;
-  @Output() routeWord = new EventEmitter<boolean>(); 
+  @Output() routeWord = new EventEmitter<boolean>();
   constructor() { }
 
   ngOnInit(): void {
